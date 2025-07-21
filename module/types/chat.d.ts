b2cc5c490fef8a8f54152ee96822cc8aba6bafe5
GabePoel <gabrielpoel@gmail.com>
2025-07-21 00:12:05 -0700

    Removed old button interface.

--- module/types/chat.d.ts
+++ module/types/chat.d.ts
@@ -6,16 +6,4 @@ export type HTMLButtonConfig = {
   icon?: string;
   type?: "button";
   disabled?: boolean;
-}
-
-/** Parameters that define a chat message button. */
-export interface ChatActionButton {
-  /** Font Awesome icon class. */
-  icon: string;
-  /** Action assigned to the button. */
-  action: string;
-  /** Label to apply to the button. */
-  label?: string;
-  /** Data to pass along with the button. */
-  data?: string;
-}
+};
