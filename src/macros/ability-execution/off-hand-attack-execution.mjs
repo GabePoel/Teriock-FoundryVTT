3a426119d92ebf412c5727b9bc059df7a434f09b
GabePoel <gabrielpoel@gmail.com>
2025-08-08 19:31:55 -0700

    Compendium update and updated multi-attack rules.

---
+++ src/macros/ability-execution/off-hand-attack-execution.mjs
@@ -0,0 +1,27 @@
+const options = foundry.utils.deepClone({
+  advantage: window.event?.altKey,
+  disadvantage: window.event?.shiftKey,
+});
+const actor = scope.abilityData.actor;
+const abilities = {};
+actor.abilities
+  .filter(
+    (a) =>
+      a.system.interaction === "attack" &&
+      a.system.maneuver === "active" &&
+      a.system.executionTime === "a1" &&
+      ["weapon", "hand"].includes(a.system.delivery.base),
+  )
+  .map((a) => (abilities[a.id] = a.name));
+const id = await game.teriock.api.dialog.select(abilities, {
+  label: "Ability",
+  hint: "Please select an ability.",
+  title: "Select Ability",
+});
+const ability = actor.abilities.find((a) => a.id === id);
+await actor.update({
+  "system.attackPenalty": Math.min(0, actor.system.attackPenalty + 3),
+});
+Hooks.once("renderChatMessageHTML", async () => {
+  await ability.system.roll(options);
+});
