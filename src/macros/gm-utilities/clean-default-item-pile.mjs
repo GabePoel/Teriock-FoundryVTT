89db4c7efd438876611446d7d5a5220877204c50
GabePoel <gabrielpoel@gmail.com>
2025-08-10 13:31:17 -0700

    Improved item piles integration.

---
+++ src/macros/gm-utilities/clean-default-item-pile.mjs
@@ -0,0 +1,8 @@
+/** @type {TeriockActor} */
+const defaultItemPile = game.actors.getName("Default Item Pile");
+if (defaultItemPile) {
+  await defaultItemPile.deleteEmbeddedDocuments(
+    "Item",
+    defaultItemPile.items.map((i) => i.id),
+  );
+}
