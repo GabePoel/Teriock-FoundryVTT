8c34198d6f1e1a7f4712c4977bc102ca0875ed7c
GabePoel <gabrielpoel@gmail.com>
2025-08-17 11:13:27 -0700

    Mechanic image.

---
+++ src/module/helpers/constants/generated/traits.mjs
@@ -0,0 +1,13 @@
+// This file was auto-generated on 2025-08-17 by scripts/update-options.mjs.
+// Do not edit manually.
+
+export const traits = {
+    animal: "Animal",
+    draconic: "Draconic",
+    humanoid: "Humanoid",
+    monstrous: "Monstrous",
+    outsider: "Outsider",
+    partialHumanoid: "Partial humanoid",
+    playerCharacter: "Player character",
+    undead: "Undead"
+};
