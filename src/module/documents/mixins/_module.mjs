948b8e4b7c03d9d1a913db1d0509d7f99b4870e1
GabePoel <gabrielpoel@gmail.com>
2025-09-16 10:38:25 -0700

    Generalized effect changes.

--- src/module/documents/mixins/_module.mjs
+++ src/module/documents/mixins/_module.mjs
@@ -1,3 +1,4 @@
 export { default as ChildDocumentMixin } from "./child-document-mixin/child-document-mixin.mjs";
 export { default as ParentDocumentMixin } from "./parent-document-mixin/parent-document-mixin.mjs";
 export { default as CommonDocumentMixin } from "./common-document-mixin/common-document-mixin.mjs";
+export { default as ChangeableDocumentMixin } from "./changeable-document-mixin/changeable-document-mixin.mjs";
