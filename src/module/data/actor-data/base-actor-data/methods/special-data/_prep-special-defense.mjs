6bbf9ddc8c16dbc630c761631cd9b4a211095bcc
GabePoel <gabrielpoel@gmail.com>
2025-10-02 02:12:34 -0700

    Natural AV.

--- src/module/data/actor-data/base-actor-data/methods/special-data/_prep-special-defense.mjs
+++ src/module/data/actor-data/base-actor-data/methods/special-data/_prep-special-defense.mjs
@@ -7,8 +7,16 @@ export function _prepSpecialDefense(actorData) {
   const armor = actorData.parent.equipment.filter(
     (e) => e.system.isEquipped && e.system.equipmentClasses.has("armor"),
   );
+  const naturalArmor = actorData.parent.bodyParts.filter(
+    (a) => !a.disabled && a.system.av.value > 0,
+  );
   if (armor.length > 0) {
     actorData.defense.av.worn = Math.max(armor.map((a) => a.system.av.value));
+  }
+  if (naturalArmor.length > 0) {
+    actorData.defense.av.natural = Math.max(
+      naturalArmor.map((a) => a.system.av.value),
+    );
   }
   actorData.defense.av.value = Math.max(
     actorData.defense.av.natural,
