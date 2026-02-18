d16e9b2356e4bf4ecf6e1f998aa075e0c91c580d
GabePoel <gabrielpoel@gmail.com>
2026-02-18 13:38:25 -0800

    Page sheets.

--- src/module/applications/sheets/_module.mjs
+++ src/module/applications/sheets/_module.mjs
@@ -2,5 +2,6 @@ export * as actor from "./actor-sheets/_module.mjs";
 export * as effect from "./effect-sheets/_module.mjs";
 export * as item from "./item-sheets/_module.mjs";
 export * as mixins from "./mixins/_module.mjs";
+export * as page from "./page-sheets/_module.mjs";
 export * as token from "./token-sheets/_module.mjs";
 export * as utility from "./utility-sheets/_module.mjs";
