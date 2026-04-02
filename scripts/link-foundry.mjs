489d939b5280f5eed54fb7fd099498add267d475
GabePoel <gabrielpoel@gmail.com>
2026-04-02 10:51:31 -0700

    Add script to help set up Foundry links.

---
+++ scripts/link-foundry.mjs
@@ -0,0 +1,38 @@
+import { exec } from "child_process";
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+let command = "";
+let SRC = "";
+const ROOT = ".";
+const DST = path.join(ROOT, "foundry");
+const PLATFORM = os.platform();
+const PATHS = ["client", "common", "templates"];
+
+if (PLATFORM === "linux") command = "zenity --file-selection --directory";
+console.log("Please select a folder where Foundry is installed.");
+
+exec(command, (err, stdout) => {
+  if (err) console.error(err);
+  if (stdout) SRC = stdout.trim();
+  if (fs.existsSync(DST)) {
+    fs.rmSync(DST, { recursive: true, force: true });
+  }
+  if (SRC) {
+    const resourcesDir = path.join(SRC, "resources");
+    if (fs.existsSync(resourcesDir)) SRC = resourcesDir;
+    const appDir = path.join(SRC, "app");
+    if (fs.existsSync(appDir)) SRC = appDir;
+    fs.mkdirSync(DST);
+    console.log("\n--- Linking Foundry Directories ---\n");
+    for (const p of PATHS) {
+      const src = path.join(SRC, p);
+      const dst = path.join(DST, p);
+      console.log(` - ${src} => ${dst}`);
+      if (fs.existsSync(src)) fs.symlinkSync(src, dst);
+    }
+  } else {
+    console.error("No valid Foundry folder selected");
+  }
+});
