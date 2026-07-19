458c9f83296e6eb7804e32382572b75f042f6124
GabePoel <gabrielpoel@gmail.com>
2026-07-19 12:04:59 -0700

    Minor actor sheet style tweaks.

--- src/module/setup/handlebars/string-helpers.mjs
+++ src/module/setup/handlebars/string-helpers.mjs
@@ -1,3 +1,13 @@
 import { dotJoin, toKebabCase } from "../../helpers/string.mjs";
 
-export default { dotJoin, toKebabCase, sign: n => n.signedString() };
+/**
+ * Show a string only if it's at least as long as some specified length.
+ * @param {string|number} value
+ * @param {number} length
+ */
+function minLen(value, length) {
+  if (typeof value === "number") { value = value.toString(); }
+  return value.length >= length ? value : "";
+}
+
+export default { dotJoin, minLen, toKebabCase, sign: n => n.signedString() };
